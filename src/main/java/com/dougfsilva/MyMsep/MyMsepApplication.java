ae169f26b9d44fad64f2225b5aa1815bd55a0f85
Douglas Ferreira da Silva <doug.ferreira.dfs@gmail.com>
2025-12-01 21:30:36 -0300

    Primeiro commit

---
+++ src/main/java/com/dougfsilva/MyMsep/MyMsepApplication.java
@@ -0,0 +1,13 @@
+package com.dougfsilva.MyMsep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMsepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMsepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dougfsilva/MyMsep/MyMsepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougfsilva.MyMsep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMsepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
